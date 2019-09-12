ffbb9438f64e1c72f369549cf6afa6becbbd6720
Esteban Jose Gonzalez Urena <b73404@ecci.ucr.ac.cr>
2019-09-12 17:35:29 -0600

    Lab #2

---
+++ Manual_ASP_NET_WEB/Manual_ASP_NET_WEB/Controllers/InformacionGeneralController.cs
@@ -0,0 +1,47 @@
+﻿using Manual_ASP_NET_WEB.Models;
+using Manual_ASP_NET_WEB.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Manual_ASP_NET_WEB.Controllers
+{
+    public class InformacionGeneralController : Controller
+    {
+        // Establece conexion con el modelo para ejecutar consultas con linq
+        private Manual_ASP_NET_DBEntities db = new Manual_ASP_NET_DBEntities();
+
+
+        // GET: InformacionGeneral
+        public ActionResult Index()
+        {
+            //Se obtienen los datos para mandarle a la vista
+            //Se obtiene la cantidad de médicos registrados
+            int cantidadMedicos = db.Medicos.Count();
+
+            //Se obtiene la cantidad de pacientes registrados
+            int cantidadPacientes = db.Pacientes.Count();
+
+            //Se obtiene la cantidad de contultas registradas
+            int cantidadConsultas = db.Consultas.Count();
+
+
+            var cantidadPacientes60 = db.Pacientes.Where(paciente => paciente.Peso > 60);
+
+            int cantidadPacientes60int = cantidadPacientes60.Count();
+
+            // Objeto con los datos obetnidos a entregar a la vista
+            var informacionObtenidaViewModel = new InformacionGeneralViewModel
+            {
+                CantidadMedicos = cantidadMedicos,
+                CantidadPacientes = cantidadPacientes,
+                CantidadConsultas = cantidadConsultas,
+                CantidadPacientes60 = cantidadPacientes60int
+            };            return View(informacionObtenidaViewModel);
+
+
+        }
+    }
+}
---
+++ Manual_ASP_NET_WEB/Manual_ASP_NET_WEB/ViewModels/InformacionGeneralViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Manual_ASP_NET_WEB.ViewModels
+{
+    public class InformacionGeneralViewModel
+    {
+        // Cantidad Pacientes
+        public int CantidadPacientes { get; set; }
+        // Cantidad Medicos
+        public int CantidadMedicos { get; set; }
+        // Cantidad Consultas
+        public int CantidadConsultas { get; set; }
+        public int CantidadPacientes60 { get; set; }
+    }
+}
