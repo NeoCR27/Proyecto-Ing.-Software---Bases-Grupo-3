685ad53adeecd2f024fd3f1c16ffcd945a88ab12
Julián Moya <julmoze24@gmail.com>
2019-10-06 16:07:40 -0600

    Simplificación codigo anterior. Se agrega barra de busqueda. Falta agregar y quitar y mostrar empleados por habilidades.

--- ProyectoPI/ProyectoPI/Models/PARTICIPA.cs
+++ ProyectoPI/ProyectoPI/Models/PARTICIPA.cs
@@ -33,11 +33,27 @@ namespace ProyectoPI.Models
         public string id { get; set;}
         [Display(Name = "Nombre Completo")]
         public string employee { get; set; }
-        [Display(Name = "Numero de Contacto")]
+        [Display(Name = "Correo de Contacto")]
         public string email { get; set; }
         [Display(Name = "Rol")]
         public string role { get; set; }
         [Display(Name = "Nombre de Proyecto")]
         public string proyName { get; set; }
+        [Display(Name = "Habilidades")]
+        public string habilidades { get; set; }
     }
+
+    public class empDesc
+    {
+        public empDesc()
+        {
+
+        }
+        [Display(Name = "ID Proyecto")]
+        public string id { get; set; }
+        [Display(Name = "Nombre Completo")]
+        public string employee { get; set; }
+    }
+
+
 }
--- ProyectoPI/ProyectoPI/Views/PARTICIPAController.cs
+++ ProyectoPI/ProyectoPI/Views/PARTICIPAController.cs
@@ -15,9 +15,7 @@ namespace ProyectoPI.Views
         private Gr03Proy4Entities db = new Gr03Proy4Entities();
         private EMPLEADOController empleados = new EMPLEADOController();
         private PROYECTOesController proyect = new PROYECTOesController();
-
-      
-
+        private HABILIDADESController habilidad = new HABILIDADESController();
 
         //Get team
         public ActionResult showTeam()
@@ -30,9 +28,8 @@ namespace ProyectoPI.Views
         public ActionResult Index()
         {
             string query = "SELECT Part.idProyectoFK AS 'id', Emp.nombre + ' ' + Emp.primerApellido + ' ' + Emp.segundoApellido AS 'employee', Emp.correo AS 'email', Part.rol AS 'role', Proy.nombre AS 'proyName' FROM PARTICIPA Part Join EMPLEADO Emp ON Emp.cedulaPK = Part.cedulaEmpleadoFK Join PROYECTO proy ON proy.idPK = Part.idProyectoFK WHERE Part.idProyectoFK = proy.idPK ORDER BY part.idProyectoFK";
-            IList<TeamViewModel> team = (db.Database.SqlQuery<TeamViewModel>(query)).ToList();
-            ViewData["teams"] = team;
-            return View();
+         
+            return View((db.Database.SqlQuery<TeamViewModel>(query)).ToList());
         }
 
         // GET: PARTICIPA/Details/5
@@ -70,65 +67,79 @@ namespace ProyectoPI.Views
         }
 
         // GET: PARTICIPA/Edit/5
-        public ActionResult Edit(string id = "2411")
+        public ActionResult Edit(string id = "2411", string searchFilter = null)
         {
-            string query = "SELECT Part.idProyectoFK AS 'ID Proyecto', Emp.nombre + ' ' + Emp.primerApellido + ' ' + Emp.segundoApellido AS 'employee', Emp.correo AS 'email', Part.rol AS 'role' FROM PARTICIPA Part Join EMPLEADO Emp ON Emp.cedulaPK = Part.cedulaEmpleadoFK WHERE Part.idProyectoFK = " + id;
-            IList<TeamViewModel> team = (db.Database.SqlQuery<TeamViewModel>(query)).ToList();
-            ViewData["team"] = team;
-            return View();
+            string queryTeam = "SELECT Part.idProyectoFK AS 'ID Proyecto', Emp.nombre + ' ' + Emp.primerApellido + ' ' + Emp.segundoApellido AS 'employee', Emp.correo AS 'email', Part.rol AS 'role' FROM PARTICIPA Part Join EMPLEADO Emp ON Emp.cedulaPK = Part.cedulaEmpleadoFK WHERE Part.idProyectoFK = " + id;
+            
+            //db.Database.SqlQuery<TeamViewModel>(query)).ToList();
+            ViewData["employees"] = (db.EMPLEADO.Where(x => x.nombre.StartsWith(searchFilter) || searchFilter == null)).ToList();
+            var tuple = new Tuple<TeamViewModel, empDesc>(new TeamViewModel(), new empDesc());
+            return View((db.Database.SqlQuery<TeamViewModel>(queryTeam)).ToList());
         }
+       /* @foreach(var employee in ViewData["employees"] as IEnumerable<ProyectoPI.Models.TeamViewModel>)
+        {
+                                         < li >
+                                             @employee.employee, @employee.email ,@employee.role
+                                         </ li >
+                                     }*/
+       /*    @foreach (var employee in ViewData["employees"] as IEnumerable<ProyectoPI.Models.TeamViewModel>)
+       {
+           <li>
+               @employee.employee, @employee.email ,@employee.role
+           </li>
+       }*/
 
-        // POST: PARTICIPA/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "rol,cedulaEmpleadoFK,idProyectoFK")] PARTICIPA pARTICIPA)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(pARTICIPA).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADO, "cedulaPK", "tel", pARTICIPA.cedulaEmpleadoFK);
-            ViewBag.idProyectoFK = new SelectList(db.PROYECTO, "idPK", "nombre", pARTICIPA.idProyectoFK);
-            return View(pARTICIPA);
-        }
+// POST: PARTICIPA/Edit/5
+// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+// más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+[HttpPost]
+[ValidateAntiForgeryToken]
+public ActionResult Edit([Bind(Include = "rol,cedulaEmpleadoFK,idProyectoFK")] PARTICIPA pARTICIPA)
+{
+if (ModelState.IsValid)
+{
+db.Entry(pARTICIPA).State = EntityState.Modified;
+db.SaveChanges();
+return RedirectToAction("Index");
+}
+ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADO, "cedulaPK", "tel", pARTICIPA.cedulaEmpleadoFK);
+ViewBag.idProyectoFK = new SelectList(db.PROYECTO, "idPK", "nombre", pARTICIPA.idProyectoFK);
+return View(pARTICIPA);
+}
 
-        // GET: PARTICIPA/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PARTICIPA pARTICIPA = db.PARTICIPA.Find(id);
-            if (pARTICIPA == null)
-            {
-                return HttpNotFound();
-            }
-            return View(pARTICIPA);
-        }
+// GET: PARTICIPA/Delete/5
+public ActionResult Delete(string id)
+{
+if (id == null)
+{
+return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+}
+PARTICIPA pARTICIPA = db.PARTICIPA.Find(id);
+if (pARTICIPA == null)
+{
+return HttpNotFound();
+}
+return View(pARTICIPA);
+}
 
-        // POST: PARTICIPA/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            PARTICIPA pARTICIPA = db.PARTICIPA.Find(id);
-            db.PARTICIPA.Remove(pARTICIPA);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+// POST: PARTICIPA/Delete/5
+[HttpPost, ActionName("Delete")]
+[ValidateAntiForgeryToken]
+public ActionResult DeleteConfirmed(string id)
+{
+PARTICIPA pARTICIPA = db.PARTICIPA.Find(id);
+db.PARTICIPA.Remove(pARTICIPA);
+db.SaveChanges();
+return RedirectToAction("Index");
+}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+protected override void Dispose(bool disposing)
+{
+if (disposing)
+{
+db.Dispose();
+}
+base.Dispose(disposing);
+}
+}
 }
