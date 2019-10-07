894fbc35baea3f2defd5cd07ce0394f46213ae4f
Julmoze <julmoze24@gmail.com>
2019-10-07 16:06:49 -0600

    Union de Equipo (Participa)

--- ProyectoPI/ProyectoPI/Models/PARTICIPA.cs
+++ ProyectoPI/ProyectoPI/Models/PARTICIPA.cs
@@ -6,6 +6,7 @@
 //     Los cambios manuales en este archivo se sobrescribirán si se regenera el código.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoPI.Models
 {
@@ -21,4 +22,38 @@ namespace ProyectoPI.Models
         public virtual EMPLEADO EMPLEADO { get; set; }
         public virtual PROYECTO PROYECTO { get; set; }
     }
+
+    public  class TeamViewModel
+    {
+        public TeamViewModel ()
+        {
+
+        }
+        [Display(Name = "ID Proyecto")]
+        public string id { get; set;}
+        [Display(Name = "Nombre Completo")]
+        public string employee { get; set; }
+        [Display(Name = "Correo de Contacto")]
+        public string email { get; set; }
+        [Display(Name = "Rol")]
+        public string role { get; set; }
+        [Display(Name = "Nombre de Proyecto")]
+        public string proyName { get; set; }
+        [Display(Name = "Habilidades")]
+        public string habilidades { get; set; }
+    }
+
+    public class empDesc
+    {
+        public empDesc()
+        {
+
+        }
+        [Display(Name = "ID Proyecto")]
+        public string id { get; set; }
+        [Display(Name = "Nombre Completo")]
+        public string employee { get; set; }
+    }
+
+
 }
