e2e73de4548038e5ed951d3ce078793094d19f2b
DESKTOP-BF4TP45\Esteban <urenaesteban27@gmail.com>
2019-10-08 14:37:31 -0600

    Movi los controladores a donde van

--- ProyectoPI/ProyectoPI/Controllers/HabilidadesController.cs
+++ ProyectoPI/ProyectoPI/Controllers/HabilidadesController.cs
@@ -7,119 +7,210 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ProyectoPI.Models;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 
-namespace ProyectoPI.Controllers
+namespace ProyectoPI.Views
 {
-    public class HabilidadesController : Controller
+    /*
+SqlConnection connection = new SqlConnection("@DataInitial Catalog= Gr03Proy4");
+SqlDataAdapter adapter = new SqlDataAdapter();
+SqlCommand command;
+DataSet d_set = new DataSet();
+DataTable d_table;
+DataRow d_row;
+
+adapter.SelectCommand = new SqlCommand("SELECT E.nombre, H.valorPK FROM EMPLEADO E JOIN HABILIDADES H ON E.cedulaPK = H.cedulaEmpleadoFK; ");
+
+adapter.Fill(d_set, "my_data");
+d_table = d_set.Tables["my_data"];
+d_row = d_table.Rows[0];
+
+/*using (var context = new BloggingContext())
+{
+    var query = context.Blogs.SqlQuery("SELECT * FROM dbo.Blogs").ToList();
+}*/
+    public class HABILIDADESController : Controller
     {
+
+        private EMPLEADOController emp_controller = new EMPLEADOController();
         private Gr03Proy4Entities db = new Gr03Proy4Entities();
 
-        // GET: Habilidades
-        public ActionResult Index()
-        {
-            var hABILIDADES = db.HABILIDADES.Include(h => h.EMPLEADO);
-            return View(hABILIDADES.ToList());
-        }
-
-        // GET: Habilidades/Details/5
-        public ActionResult Details(string id)
+        // GET: HABILIDADES
+        public ActionResult Index(String id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            HABILIDADE hABILIDADE = db.HABILIDADES.Find(id);
-            if (hABILIDADE == null)
-            {
-                return HttpNotFound();
-            }
-            return View(hABILIDADE);
+            //var hABILIDADES = db.HABILIDADES.Include(h => h.EMPLEADO);
+            var hABILIDADES = db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id);
+            SelectList nombre = emp_controller.get_nombres(id);
+            ViewBag.nombre = nombre;
+            return View(hABILIDADES.ToList());
         }
 
-        // GET: Habilidades/Create
-        public ActionResult Create()
+        // GET: HABILIDADES/Details/5
+        public ActionResult Details(string id)
         {
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADOes, "cedulaPK", "tel");
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            //string sql_query = "SELECT valorPK FROM HABILIDADES WHERE 1 = cedulaEmpleadoFK;";
+            //List<String> query = db.Database.SqlQuery<String>(sql_query).ToList();
+            ViewBag.habilidades = new SelectList(db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id), "", "valorPK");
+
+            SelectList nombre = emp_controller.get_nombres(id);
+            ViewBag.nombre = nombre;
+
             return View();
         }
 
-        // POST: Habilidades/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        public ActionResult detalles_empleado(string id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            string sql_query = "SELECT valorPK, tipoPK FROM HABILIDADES WHERE cedulaEmpleadoFK = 1;";
+            //IList<String> query = db.Database.SqlQuery<String>(sql_query).ToList();
+
+            ViewBag.habilidades = new SelectList(db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id), "", "valorPK", "tipoPK");
+
+            //IList<HABILIDADES> habilidades = (db.Database.SqlQuery<HABILIDADES>(sql_query)).ToList();
+            //ViewData["data"] = habilidades;
+
+            SelectList nombre = emp_controller.get_nombres(id);
+            ViewBag.nombre = nombre;
+
+            return View();
+        }
+
+        // GET: HABILIDADES/Create
+        public ActionResult Create()
+        {
+            var hABILIDADES = db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == "1");
+            ViewBag.cedulaEmpleadoFK = emp_controller.get_cedulas();
+            string[] values = new[] { "Tecnica", "Blanda" };
+            ViewBag.tipoPK = new SelectList(values);
+
+            //return View(hABILIDADES.ToList());
+            return View();
+
+        }
+
+        // POST: HABILIDADES/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "valorPK,tipoPK,cedulaEmpleadoFK")] HABILIDADE hABILIDADE)
+        public ActionResult Create([Bind(Include = "valorPK,tipoPK,cedulaEmpleadoFK")] HABILIDADES hABILIDADES)
         {
             if (ModelState.IsValid)
             {
-                db.HABILIDADES.Add(hABILIDADE);
+                db.HABILIDADES.Add(hABILIDADES);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("../EMPLEADO/index");
             }
 
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADOes, "cedulaPK", "tel", hABILIDADE.cedulaEmpleadoFK);
-            return View(hABILIDADE);
+            //ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADO, "cedulaPK", "tel", hABILIDADES.cedulaEmpleadoFK);
+            return View(hABILIDADES);
         }
 
-        // GET: Habilidades/Edit/5
+        // GET: HABILIDADES/Edit/5
         public ActionResult Edit(string id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            HABILIDADE hABILIDADE = db.HABILIDADES.Find(id);
-            if (hABILIDADE == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADOes, "cedulaPK", "tel", hABILIDADE.cedulaEmpleadoFK);
-            return View(hABILIDADE);
+            var hABILIDADES = db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id);
+            //ViewBag.datos = new SelectList(db.HABILIDADES.Where(x=>x.cedulaEmpleadoFK == id), "cedulaEmpleadoFK", "tipoPK", "valorPK");
+
+            //ViewBag.cedulaEmpleadoFK = new SelectList(db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id), "", "valorPK", "tipoPK");
+            return View(hABILIDADES.ToList());
         }
 
-        // POST: Habilidades/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: HABILIDADES/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598. [Bind(Include = "valorPK,tipoPK,cedulaEmpleadoFK")] HABILIDADES hABILIDADES 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "valorPK,tipoPK,cedulaEmpleadoFK")] HABILIDADE hABILIDADE)
+        public ActionResult Edit(FormCollection collection)
         {
-            if (ModelState.IsValid)
+            foreach (string key in collection.AllKeys)
             {
-                db.Entry(hABILIDADE).State = EntityState.Modified;
+                Response.Write("Key = " + key);
+                Response.Write(collection[key]);
+                Response.Write("<br/>");
+
+            }
+            /*if (ModelState.IsValid)
+            {
+                db.Entry(hABILIDADES).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADOes, "cedulaPK", "tel", hABILIDADE.cedulaEmpleadoFK);
-            return View(hABILIDADE);
+            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADO, "cedulaPK", "tel", hABILIDADES.cedulaEmpleadoFK);
+            */
+            return View();
         }
 
-        // GET: Habilidades/Delete/5
-        public ActionResult Delete(string id)
+        // GET: HABILIDADES/Delete/5
+        public ActionResult Delete(string id, string valor, string tipo)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            HABILIDADE hABILIDADE = db.HABILIDADES.Find(id);
-            if (hABILIDADE == null)
-            {
-                return HttpNotFound();
-            }
-            return View(hABILIDADE);
+
+            //string query = "SELECT * FROM HABILIDADES WHERE tipoPK = @tipo AND valorPK = @valor AND cedulaEmpleadoFK = @id";
+            ViewBag.habilidades = new SelectList(db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id && x.tipoPK == tipo && x.valorPK == valor), "cedulaEmpleadoFK", "tipoPK", "valorPK");
+
+            var hABILIDADES = db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id && x.tipoPK == tipo && x.valorPK == valor);
+
+            return View(hABILIDADES);
         }
 
-        // POST: Habilidades/Delete/5
+        // POST: HABILIDADES/Delete/5
+
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
+        public async Task<ActionResult> DeleteConfirmed(string id, string valor, string tipo)
         {
-            HABILIDADE hABILIDADE = db.HABILIDADES.Find(id);
-            db.HABILIDADES.Remove(hABILIDADE);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+
+            //var hABILIDADES = db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id && x.tipoPK == tipo && x.valorPK == valor);
+            //Console.WriteLine(id, valor);
+
+            //HABILIDADES hABILIDADES = db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id);
+            HABILIDADES hABILIDADES = await db.HABILIDADES.FindAsync(id, tipo, valor);
+            db.HABILIDADES.Remove(hABILIDADES);
+            await db.SaveChangesAsync();
+            //ViewBag.habilidades = new SelectList(db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id && x.tipoPK == tipo && x.valorPK == valor), "cedulaEmpleadoFK", "tipoPK", "valorPK");
+
+            //return View(hABILIDADES);
+            return RedirectToAction("Index", new { id = id });
         }
 
+        public ActionResult to_empleados(string id)
+        {
+            return RedirectToAction("../EMPLEADO/Index", new { });
+        }
+
+        /*public async Task<ActionResult> DeleteConfirmed(string id, string valor, string tipo)
+        {
+
+            HABILIDADES habilidad = await db.HABILIDADES.FindAsync(id, valor, tipo);
+            //HABILIDADES hABILIDADES = db.HABILIDADES.Where(x => x.cedulaEmpleadoFK == id);
+            db.HABILIDADES.Remove(habilidad);
+            await db.SaveChangesAsync();
+            return RedirectToAction("../EMPLEADO/Index", new { id =id });
+        }*/
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- ProyectoPI/ProyectoPI/Controllers/ParticipaController.cs
+++ ProyectoPI/ProyectoPI/Controllers/ParticipaController.cs
@@ -8,129 +8,138 @@ using System.Web;
 using System.Web.Mvc;
 using ProyectoPI.Models;
 
-namespace ProyectoPI.Controllers
+namespace ProyectoPI.Views
 {
-    public class ParticipaController : Controller
+    public class PARTICIPAController : Controller
     {
         private Gr03Proy4Entities db = new Gr03Proy4Entities();
+        private EMPLEADOController empleados = new EMPLEADOController();
+        private PROYECTOesController proyect = new PROYECTOesController();
+        private HABILIDADESController habilidad = new HABILIDADESController();
 
-        // GET: Participa
+        //Get team
+        public ActionResult showTeam()
+        {
+            var pARTICIPA = db.PARTICIPA.Include(p => p.EMPLEADO).Include(p => p.PROYECTO);
+            return View(pARTICIPA.ToList());
+        }
+
+        // GET: PARTICIPA
         public ActionResult Index()
         {
-            var pARTICIPAs = db.PARTICIPAs.Include(p => p.EMPLEADO).Include(p => p.PROYECTO);
-            return View(pARTICIPAs.ToList());
+            string query = "SELECT Part.idProyectoFK AS 'id', Emp.nombre + ' ' + Emp.primerApellido + ' ' + Emp.segundoApellido AS 'employee', Emp.correo AS 'email', Part.rol AS 'role', Proy.nombre AS 'proyName' FROM PARTICIPA Part Join EMPLEADO Emp ON Emp.cedulaPK = Part.cedulaEmpleadoFK Join PROYECTO proy ON proy.idPK = Part.idProyectoFK WHERE Part.idProyectoFK = proy.idPK ORDER BY part.idProyectoFK";
+         
+            return View((db.Database.SqlQuery<TeamViewModel>(query)).ToList());
         }
 
-        // GET: Participa/Details/5
-        public ActionResult Details(string id)
+        // GET: PARTICIPA/Details/5
+        public ActionResult Details()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PARTICIPA pARTICIPA = db.PARTICIPAs.Find(id);
-            if (pARTICIPA == null)
-            {
-                return HttpNotFound();
-            }
-            return View(pARTICIPA);
-        }
-
-        // GET: Participa/Create
-        public ActionResult Create()
-        {
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADOes, "cedulaPK", "tel");
-            ViewBag.idProyectoFK = new SelectList(db.PROYECTOes, "idPK", "nombre");
+         
             return View();
         }
 
-        // POST: Participa/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        // GET: PARTICIPA/Create
+        public ActionResult Create()
+        {
+            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADO, "cedulaPK", "tel");
+            ViewBag.idProyectoFK = new SelectList(db.PROYECTO, "idPK", "nombre");
+            return View();
+        }
+
+        // POST: PARTICIPA/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "rol,cedulaEmpleadoFK,idProyectoFK")] PARTICIPA pARTICIPA)
         {
             if (ModelState.IsValid)
             {
-                db.PARTICIPAs.Add(pARTICIPA);
+                db.PARTICIPA.Add(pARTICIPA);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADOes, "cedulaPK", "tel", pARTICIPA.cedulaEmpleadoFK);
-            ViewBag.idProyectoFK = new SelectList(db.PROYECTOes, "idPK", "nombre", pARTICIPA.idProyectoFK);
+            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADO, "cedulaPK", "tel", pARTICIPA.cedulaEmpleadoFK);
+            ViewBag.idProyectoFK = new SelectList(db.PROYECTO, "idPK", "nombre", pARTICIPA.idProyectoFK);
             return View(pARTICIPA);
         }
 
-        // GET: Participa/Edit/5
-        public ActionResult Edit(string id)
+        // GET: PARTICIPA/Edit/5
+        public ActionResult Edit(string id = "2411", string searchFilter = null)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PARTICIPA pARTICIPA = db.PARTICIPAs.Find(id);
-            if (pARTICIPA == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADOes, "cedulaPK", "tel", pARTICIPA.cedulaEmpleadoFK);
-            ViewBag.idProyectoFK = new SelectList(db.PROYECTOes, "idPK", "nombre", pARTICIPA.idProyectoFK);
-            return View(pARTICIPA);
+            string queryTeam = "SELECT Part.idProyectoFK AS 'ID Proyecto', Emp.nombre + ' ' + Emp.primerApellido + ' ' + Emp.segundoApellido AS 'employee', Emp.correo AS 'email', Part.rol AS 'role' FROM PARTICIPA Part Join EMPLEADO Emp ON Emp.cedulaPK = Part.cedulaEmpleadoFK WHERE Part.idProyectoFK = " + id;
+            
+            //db.Database.SqlQuery<TeamViewModel>(query)).ToList();
+            ViewData["employees"] = (db.EMPLEADO.Where(x => x.nombre.StartsWith(searchFilter) || searchFilter == null)).ToList();
+           // var tuple = new Tuple<TeamViewModel, empDesc>(new TeamViewModel(), new empDesc());
+            return View((db.Database.SqlQuery<TeamViewModel>(queryTeam)).ToList());
         }
+       /* @foreach(var employee in ViewData["employees"] as IEnumerable<ProyectoPI.Models.TeamViewModel>)
+        {
+                                         < li >
+                                             @employee.employee, @employee.email ,@employee.role
+                                         </ li >
+                                     }*/
+       /*    @foreach (var employee in ViewData["employees"] as IEnumerable<ProyectoPI.Models.TeamViewModel>)
+       {
+           <li>
+               @employee.employee, @employee.email ,@employee.role
+           </li>
+       }*/
 
-        // POST: Participa/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "rol,cedulaEmpleadoFK,idProyectoFK")] PARTICIPA pARTICIPA)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(pARTICIPA).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADOes, "cedulaPK", "tel", pARTICIPA.cedulaEmpleadoFK);
-            ViewBag.idProyectoFK = new SelectList(db.PROYECTOes, "idPK", "nombre", pARTICIPA.idProyectoFK);
-            return View(pARTICIPA);
-        }
+// POST: PARTICIPA/Edit/5
+// Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+// más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+[HttpPost]
+[ValidateAntiForgeryToken]
+public ActionResult Edit([Bind(Include = "rol,cedulaEmpleadoFK,idProyectoFK")] PARTICIPA pARTICIPA)
+{
+if (ModelState.IsValid)
+{
+db.Entry(pARTICIPA).State = EntityState.Modified;
+db.SaveChanges();
+return RedirectToAction("Index");
+}
+ViewBag.cedulaEmpleadoFK = new SelectList(db.EMPLEADO, "cedulaPK", "tel", pARTICIPA.cedulaEmpleadoFK);
+ViewBag.idProyectoFK = new SelectList(db.PROYECTO, "idPK", "nombre", pARTICIPA.idProyectoFK);
+return View(pARTICIPA);
+}
 
-        // GET: Participa/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PARTICIPA pARTICIPA = db.PARTICIPAs.Find(id);
-            if (pARTICIPA == null)
-            {
-                return HttpNotFound();
-            }
-            return View(pARTICIPA);
-        }
+// GET: PARTICIPA/Delete/5
+public ActionResult Delete(string id)
+{
+if (id == null)
+{
+return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+}
+PARTICIPA pARTICIPA = db.PARTICIPA.Find(id);
+if (pARTICIPA == null)
+{
+return HttpNotFound();
+}
+return View(pARTICIPA);
+}
 
-        // POST: Participa/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            PARTICIPA pARTICIPA = db.PARTICIPAs.Find(id);
-            db.PARTICIPAs.Remove(pARTICIPA);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+// POST: PARTICIPA/Delete/5
+[HttpPost, ActionName("Delete")]
+[ValidateAntiForgeryToken]
+public ActionResult DeleteConfirmed(string id)
+{
+PARTICIPA pARTICIPA = db.PARTICIPA.Find(id);
+db.PARTICIPA.Remove(pARTICIPA);
+db.SaveChanges();
+return RedirectToAction("Index");
+}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+protected override void Dispose(bool disposing)
+{
+if (disposing)
+{
+db.Dispose();
+}
+base.Dispose(disposing);
+}
+}
 }
