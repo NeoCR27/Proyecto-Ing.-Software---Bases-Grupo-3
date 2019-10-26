e940ab977ec41e59625103cac4a84a9822042851
pablo cheng <pablocheng18@gmail.com>
2019-10-26 14:19:06 -0600

    agregar modelos con formato

--- ProyectoPI/ProyectoPI/Models/CLIENTE.cs
+++ ProyectoPI/ProyectoPI/Models/CLIENTE.cs
@@ -11,7 +11,8 @@ namespace ProyectoPI.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class CLIENTE
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,18 +20,61 @@ namespace ProyectoPI.Models
         {
             this.PROYECTO = new HashSet<PROYECTO>();
         }
-    
-        public string cedulaPK { get; set; }
-        public string tel { get; set; }
+
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Solo se pueden digitar letras")]
+        [MaxLength(20, ErrorMessage = "El máximo de caracteres es 20")]
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [Display(Name = "Nombre")]
         public string nombre { get; set; }
+
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Solo se pueden digitar letras")]
+        [MaxLength(20, ErrorMessage = "El máximo de caracteres es 20")]
+        [Required(ErrorMessage = "El campo Primer Apellido es obligatorio")]
+        [Display(Name = "Primer Apellido")]
         public string primerApellido { get; set; }
+
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Solo se pueden digitar letras")]
+        [MaxLength(20, ErrorMessage = "El máximo de caracteres es 20")]
+        [Display(Name = "Segundo Apellido")]
         public string segundoApellido { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Solo se pueden digitar numeros")]
+        [MaxLength(20, ErrorMessage = "El máximo de caracteres es 20")]
+        [Required(ErrorMessage = "El campo Cédula es obligatorio")]
+        [Display(Name = "Cédula")]
+        public string cedulaPK { get; set; }
+
+        [MaxLength(15, ErrorMessage = "El máximo de caracteres es 15")]
+        [Required(ErrorMessage = "El campo Teléfono es obligatorio")]
+        [Display(Name = "Teléfono")]
+        public string tel { get; set; }
+
+        [MaxLength(40, ErrorMessage = "El máximo de caracteres es 40")]
+        [EmailAddress(ErrorMessage = "El campo Correo tiene que ser un correo válido")]
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
+        [Display(Name = "Correo")]
         public string correo { get; set; }
-        public string distrito { get; set; }
-        public string canton { get; set; }
+
+        [MaxLength(20, ErrorMessage = "El máximo de caracteres es 20")]
+        [Required(ErrorMessage = "El campo Provincia es obligatorio")]
+        [Display(Name = "Provincia")]
         public string provincia { get; set; }
+
+        [MaxLength(20, ErrorMessage = "El máximo de caracteres es 20")]
+        [Required(ErrorMessage = "El campo Cantón es obligatorio")]
+        [Display(Name = "Cantón")]
+        public string canton { get; set; }
+
+        [MaxLength(20, ErrorMessage = "El máximo de caracteres es 20")]
+        [Required(ErrorMessage = "El campo Distrito es obligatorio")]
+        [Display(Name = "Distrito")]
+        public string distrito { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [MaxLength(200, ErrorMessage = "El máximo de caracteres es 200")]
+        [Display(Name = "Dirección Exacta")]
         public string direccionExacta { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PROYECTO> PROYECTO { get; set; }
     }
--- ProyectoPI/ProyectoPI/Models/EMPLEADO.cs
+++ ProyectoPI/ProyectoPI/Models/EMPLEADO.cs
@@ -6,6 +6,7 @@
 //     Los cambios manuales en este archivo se sobrescribirán si se regenera el código.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoPI.Models
 {
@@ -22,23 +23,78 @@ namespace ProyectoPI.Models
             this.REQUERIMIENTOS = new HashSet<REQUERIMIENTOS>();
             this.REQUERIMIENTOS1 = new HashSet<REQUERIMIENTOS>();
         }
-    
+
+
+        [Required]
+        [MaxLength(9)]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Cedula constituida unicamente por numeros")]
+        [Display(Name = "Cedula")]
         public string cedulaPK { get; set; }
+
+        [Required]
+        [Phone]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Telefono constituido unicamente por valores numericos")]
+        [Display(Name = "Telefono")]
         public string tel { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage = "Nombre no puede contener valores numericos")]
+        [Display(Name = "Nombre")]
         public string nombre { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage = "Apellido no puede contener valores numericos")]
+        [Display(Name = "Primer apellido")]
         public string primerApellido { get; set; }
+
+
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage = "Apellido no puede contener valores numericos")]
+        [Display(Name = "Segundo apellido")]
         public string segundoApellido { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo")]
         public string correo { get; set; }
+
+        [Required]
+        [Display(Name = "Distrito")]
         public string distrito { get; set; }
+
+        [Required]
+        [Display(Name = "Canton")]
         public string canton { get; set; }
+
+        [Required]
+        [Display(Name = "Provincia")]
         public string provincia { get; set; }
+
+
+        [Display(Name = "Direccion exacta")]
         public string direccionExacta { get; set; }
+
+        //[Required] SE CAE!!!!!
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Horas laboradas constituida unicamente por valores numericos")]
+        [Display(Name = "Horas laboradas")]
         public Nullable<int> horasLaboradas { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Edad constituida unicamente por valores numericos")]
+        [Display(Name = "Edad")]
         public int edad { get; set; }
+
+        [Required]
+        [Display(Name = "Disponibilidad")]
         public Nullable<bool> disponibilidad { get; set; }
+
+        [Display(Name = "Rol")]
         public string rol { get; set; }
+
+        [Required]
+        [Display(Name = "Fecha de nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public System.DateTime fechaNacimiento { get; set; }
-    
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<HABILIDADES> HABILIDADES { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
