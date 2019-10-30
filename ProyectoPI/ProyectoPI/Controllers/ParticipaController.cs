4ec5a835813173260094a82c80d9092e7bf00528
Julián Moya <julmoze24@gmail.com>
2019-10-29 18:55:13 -0600

    Arreglo de inyección en barra de busqueda. Se agrega un p. almacenado en la BD y se modifica el codigo en PARTICIPA para incorporar el cambio

--- ProyectoPI/ProyectoPI/Controllers/ParticipaController.cs
+++ ProyectoPI/ProyectoPI/Controllers/ParticipaController.cs
@@ -81,7 +81,7 @@ namespace ProyectoPI.Controllers
             }
         }
         // GET: PARTICIPA/Edit/5
-        public async Task<ActionResult> Edit(string id = "2411", string buscarPor = "", string filtroBusqueda = "")
+        public async Task<ActionResult> Edit(string id, string buscarPor = "", string filtroBusqueda = "")
         {
             string user = User.Identity.Name;
             string rol = await this.seguridadController.GetRol(user); // Me retorna el rol del usuario logeado
@@ -89,7 +89,7 @@ namespace ProyectoPI.Controllers
             //Se Arma los  string de query
             ViewBag.id = id;
             string queryEquipo = "SELECT Part.idProyectoFK AS 'IDProyecto', Emp.nombre + ' ' + Emp.primerApellido AS 'empleado', Emp.correo AS 'email', Part.rol AS 'rol', Emp.disponibilidad AS 'dispon', Emp.cedulaPK AS 'idEmp', Proy.nombre AS 'proyNom' FROM PARTICIPA Part Join EMPLEADO Emp ON Emp.cedulaPK = Part.cedulaEmpleadoFK JOIN Proyecto Proy ON Proy.idPK = Part.idProyectoFK WHERE Part.idProyectoFK = " + id;
-            string queryEmpleado = "Select Distinct emp.cedulaPK AS 'personalID', emp.Nombre + ' ' + emp.primerApellido AS 'nombre' , hab.tipoPK AS 'tipoHabilidad' , hab.valorPK AS 'habilidad' From EMPLEADO emp Join HABILIDADES hab ON emp.cedulaPK = hab.cedulaEmpleadoFK WHERE hab.tipoPK LIKE '%" + buscarPor + "%' AND hab.valorPK LIKE '%" + filtroBusqueda + "%' AND Emp.rol != 'Lider' AND Emp.rol != 'Jefe' AND Emp.Disponibilidad != 'No Disponible' Order BY emp.cedulaPK";
+            string queryEmpleado = ("Exec recuperarHabilidad" + "'"+ buscarPor + "','" + filtroBusqueda + "'");
             //Se hace el query a la base de datos
             IList<EquipoModel> resultadoQueryEquipo = (db.Database.SqlQuery<EquipoModel>(queryEquipo)).ToList();
             IList<HabilidadEmpleadoModel> resultadoQueryEmpleado = (db.Database.SqlQuery<HabilidadEmpleadoModel>(queryEmpleado)).ToList();
